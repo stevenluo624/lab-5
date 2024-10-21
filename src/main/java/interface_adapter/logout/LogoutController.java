1378cfa43e2ba014d94eec898baffa104f06c8cd
Tianyu Luo <tianyuluo@Tianyus-MacBook-Air.local>
2024-10-21 18:13:31 -0400

    deleting TODOs in logout inputdata and controller

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -20,7 +20,6 @@ public class LogoutController {
      * @param username the username of the user logging in
      */
     public void execute(String username) {
-        // TODO: run the use case interactor for the logout use case
         // 1. instantiate the `LogoutInputData`, which should contain the username.
         // 2. tell the Interactor to execute.
         final LogoutInputData logoutInputData = new LogoutInputData(username);
